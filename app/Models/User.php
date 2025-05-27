0672e293fc07e11eb4da22d5a0c237599244b577
glg2013 <57412960@qq.com>
2025-05-27 04:49:47 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -96,4 +96,10 @@ class User extends Authenticatable
         }
         $this->followings()->detach($user_ids);
     }
+
+    public function isFollowing($user_id)
+    {
+        //return $this->followings()->contains($user_id);
+        return $this->followings()->where('user_id', $user_id)->exists();
+    }
 }
