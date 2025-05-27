f9014cd5c4a5eaa4f839104118ce07ec2455aab4
glg2013 <57412960@qq.com>
2025-05-27 02:54:57 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,6 +24,15 @@ class StatusesController extends Controller
         ]);
 
         session()->flash('success', '发不成功！');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+
+        $status->delete();
+        session()->flash('success', '微博已被删除成功！');
         return redirect()->back();
     }
 }
